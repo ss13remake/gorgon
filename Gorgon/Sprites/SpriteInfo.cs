b36c4609ab9b170cef2b0bee5139a31ff829723c
nyxation <nyxation@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2012-02-06 00:40:19 +0000

    Adding SpriteInfo to Gorgon library so it can be removed from the ResourceManager class file and be more globally used without having to reference ClientServices.

---
+++ Gorgon/Sprites/SpriteInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace GorgonLibrary.Sprites
+{
+    /// <summary>
+    /// Struct to hold assorted sprite related information,
+    /// such as the name, offsets, and size.
+    /// </summary>
+    public struct SpriteInfo
+    {
+        public string Name;
+        public Vector2D Offsets;
+        public Vector2D Size;
+    }
+}
